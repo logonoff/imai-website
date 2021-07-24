cb2b5a7b2406ba0412935bb4602ff22bde116632
logonoff <logonoff@users.noreply.github.com>
2021-07-24 02:38:49 -0400

    rewrite the menu
    time logged: 3:00:07

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 $.fn.addSelfLink = function() {
 	return this.each(function() {
 		$(this).append(" " +
-			"<a class='self-link instapaper_hide instapaper_ignore' href='#" + $(this).attr("id") +
+			"<a class='self-link instapaper_hide instapaper_ignore' href='#" + $(this).closest("[id]").attr("id") +
 			"' aria-hidden='true' tabindex='-1' title='Permalink to this section'>#</a>"
 		);
 	});
@@ -10,3 +10,11 @@ $.fn.addSelfLink = function() {
 
 // Apply self link to h2 and h2 w. js-self-link class
 $(".js-self-link h2, .js-self-link h3").addSelfLink();
+
+// filter menu items
+$("#search").on("keyup", function() {
+	var value = $(this).val().toLowerCase();
+	$("#menu .name").filter(function() {
+		$(this).closest(".dish").toggle($(this).text().toLowerCase().indexOf(value) > -1);
+	}); 
+});
