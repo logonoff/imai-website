13329dc374948325caeed88acf587c65f674d9c5
logonoff <logonoff@users.noreply.github.com>
2021-07-29 14:53:55 -0400

    add menu categorization filters
    time logged: 01:12:41

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,32 @@ $.fn.addSelfLink = function() {
 	});
 };
 
+
 // Apply self link to h2 and h2 w. js-self-link class
 $(".js-self-link h2, .js-self-link h3").addSelfLink();
 
-// filter menu items
-$("#search-menu").on("keyup", function() {
-	var value = $(this).val().toLowerCase();
-	$("#menu .dish").filter(function() {
-		$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-	}); 
+/*
+ * Menu
+ */
+// only show search when js is loaded
+$("#search-menu").show();
 
+// filter all dishes by class
+function filterSelection(c) {
+	var x, i;
+	x = document.getElementsByClassName("dish");
+	if (c == "all") c = "";
+
+	for (i = 0; i < x.length; i++) {
+		$(x[i]).hide();
+		if (x[i].className.indexOf(c) > -1) {
+			$(x[i]).show();
+		};
+	}
+}
+
+// hide section of the menu that are all hidden
+function hideEmptySections() {
 	// hide empty sections
 	jQuery.each($("#menu").children(), function() {
 		if ($("#" + $(this).closest("section").attr("id") + " .dish:not([style*='display: none'])").length) {
@@ -33,4 +49,31 @@ $("#search-menu").on("keyup", function() {
 	} else {
 		$("#empty-msg").hide();
 	}
+}
+
+// filter by search
+$("#search-menu").on("keyup", function() {
+	var value = $(this).val().toLowerCase();
+	$("#menu .dish").filter(function() {
+		$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+	}); 
+
+	hideEmptySections();
 });
+
+// filter by category
+$('#filter-selection').click(function() {
+	var filter = ""
+
+	if ($("#vegetarian").prop("checked")) {
+		filter += "vegetarian";
+	}
+
+	if ($("#spicy").prop("checked")) {
+		filter += " spicy";
+	}
+
+	filterSelection(filter);
+	hideEmptySections();
+});
+
